3a793b9b643cafc79316511dfd6af19234a03311
ajkfrye <ajkfrye@gmail.com>
2024-11-21 20:45:25 -0800

    finish this shit for real

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    #print(text)
-    
-    num_of_words = word_count(text)
-    #print(f'{num_of_words} words')
-    
-    char_counts = char_count(text)
-    #print(char_dict)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-    print('--- Begin report of books/frankenstein.txt ---')
-    print(f'{word_count(text)} words found in the document\n')
-    print(char_report(char_count(text)))
-    print('--- End report ---')
-    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def word_count(text):
-    list = text.split()
-    count = 0
-    for word in list:
-        count += 1
-    return count
 
-def char_count(text):
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
     chars = {}
     for c in text:
         lowered = c.lower()
@@ -37,8 +44,10 @@ def char_count(text):
             chars[lowered] = 1
     return chars
 
-def char_report(chars):
-    for char in chars:
-        print(f"the '{char}' character was found {chars[char]} times")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
